1a599b2be42aaccb75bb2604907a694997dd9cb3
Dylan Fernandes <majestickookie@gmail.com>
2022-04-26 22:52:53 +0100

    Implemented method to create greeting packet and send to client. Further implemented is_ack() method to check if packets received are acknowledgement packets. Also implemented code to receive acknowledgement packets from the client and outputs relevant message if corrupt or received successfully.

--- festivalserver.py
+++ festivalserver.py
@@ -11,6 +11,7 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('127.0.0.1', 13000))
 print("SERVER RUNNING")
 
+
 # Hardcode all ips that will be used
 
 
@@ -34,7 +35,7 @@ def is_corrupt(packet):
 
     # Recreate packet without the checksum number at the end
 
-    #seq_num = int.from_bytes(packet[0:4], "big")
+    # seq_num = int.from_bytes(packet[0:4], "big")
     seq_num = packet[0:4]
     ack_flag = packet[4:8]
     pay_len = packet[8:12]
@@ -56,11 +57,11 @@ def is_corrupt(packet):
 
 
 def checksum(pkt):
-
     h = hashlib.new('md5')
     h.update(pickle.dumps(pkt))
 
     return h.hexdigest()
+
 
 def create_packet():
     # Creating packet with the format
@@ -98,6 +99,50 @@ def create_packet():
     return pkt
 
 
+def create_greeting_packet(seq, ack, payload):
+    seq_num = seq
+    ack_flag = ack
+    payload = "{}".format(payload)
+    pay_len = len(payload.encode("ascii"))
+
+    # Need to pad the payload with extra characters since it is not at the maximum number of bytes
+    # Padding payload with f character
+    if len(payload) < 20:
+        temp = len(payload)
+        for i in range(temp, 20, 1):
+            payload = payload + "f"
+
+    payload = payload.encode("ascii")
+
+    # Converting each number to 4 bytes as specified in RFC format
+    seq_num = seq_num.to_bytes(4, byteorder="big")
+    ack_flag = ack_flag.to_bytes(4, byteorder="big")
+    pay_len = pay_len.to_bytes(4, byteorder="big")
+
+    # Sequence number, Ack flag, payload length, payload, checkSum
+
+    pkt_no_checksum = seq_num + ack_flag + pay_len + payload
+    checksum_num = checksum(pkt_no_checksum)
+
+    checksum_num = checksum_num.encode('ascii')
+
+    # Concatenate all together to create packet
+    pkt = seq_num + ack_flag + pay_len + payload + checksum_num
+
+    return pkt
+
+
+def is_ack(packet):
+    # Sequence number, Ack flag, payload length, payload, checkSum
+
+    ack_flag = packet[4:8]
+
+    if ack_flag == 1:
+        return True
+    else:
+        return False
+
+
 while True:
     # Receiving packet from the client
     client_data = serverSocket.recvfrom(1024)
@@ -106,8 +151,6 @@ while True:
     # packet from client
     packet = client_data[0]
 
-
-
     # save the address it has received it from
     address = client_data[1]
 
@@ -115,10 +158,28 @@ while True:
     # If is_corrupt is false then the packet is in its correct state
 
     if not is_corrupt(packet):
-        print("TEST SUCCESSFUL PACKET IS NOT CORRUPTED")
-        print("SENDING ACKNOWLEDGEMENT PACKET")
+        print("[FESTIVAL REQUEST RECEIVED]")
+        print("[SENDING ACKNOWLEDGEMENT PACKET]")
         # Sending acknowledgement packet to the client
         serverSocket.sendto(create_packet(), address)
 
     else:
-        print("CORRUPTED")
+        print("[CORRUPTED]")
+
+    # Send greeting
+    pay_len = int.from_bytes(packet[8:12], "big")
+    payload = "Merry " + packet[12:32].decode("ascii")[:pay_len]
+    print("[SENDING GREETING]")
+    serverSocket.sendto(create_greeting_packet(0, 0, payload), address)
+
+    client_ack = serverSocket.recvfrom(1024)[0]
+
+    if is_corrupt(client_ack):
+        print("[PACKET RECEIVED WAS CORRUPTED]")
+    elif is_ack(client_ack):
+        print("[PACKET RECEIVED WAS NOT AN ACKNOWLEDGEMENT]")
+    else:
+        print("[ACKNOWLEDGEMENT RECEIVED]")
+
+
+
