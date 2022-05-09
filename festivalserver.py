4fba141f3a330d8d1cc31ef2d833efd561f234a4
Dylan Fernandes <majestickookie@gmail.com>
2022-05-09 13:59:23 +0100

    Corrected multitude of errors, implemented sequence mismatch and max number of tries.
    - Implemented a list of whitelisted Ips that can connect to the server
    - When creating acknowledgement packet the sequence number is now a randomly generated number (1,100)
    - Implemented a counter for the maximum number of times the client will send the festival request
    - Changed method called in checksum method to generate checksum
    - Correctly implemented sequence mismatch
    - Created an if statement check for sequence mismatch

--- festivalserver.py
+++ festivalserver.py
@@ -2,33 +2,21 @@ import hashlib
 import pickle
 from struct import *
 from socket import *
+from random import *
 
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Assign IP address and port number to socket
-serverSocket.bind(('127.0.0.1', 10000))
+serverSocket.bind(('127.0.0.1', 1062))
 print("SERVER RUNNING")
 
 # List of whitelisted IPs
-listOfIps = ['', '', '', '', '', '']
+listOfIps = ['192.168.1.72', '192.168.86.70', '192.168.1.253']
 
 # Dictionary of festivals  Key: Value
-festival_dict = {"Christmas": "Merry Christmas", "Diwali": "Happy Diwali", "Easter": "Happy Easter", "Eid": "Eid Mubarak"}
-
-
-def rdt_recv(rcvpkt):
-    # Sequence number, Ack flag, payload length, payload, checkSum
-
-    packet = unpack("i i i 9s 32s ", rcvpkt)
-
-    # Assigning each item from tuple to appropriate variable
-    seq_num = packet[0]
-    ack_flag = packet[1]
-    pay_len = packet[2]
-
-    payload = packet[3].decode("ascii")
-    check_sum = packet[4].decode("ascii")
+festival_dict = {"Christmas": "Merry Christmas", "Diwali": "Happy Diwali", "Easter": "Happy Easter",
+                 "Eid": "Eid Mubarak"}
 
 
 def festival_reply(festival):
@@ -38,7 +26,7 @@ def festival_reply(festival):
                 return festival_dict.get(key)
 
     else:
-        print("Unfortunately your desired festival {} is not supported with our protocol".format(festival.upper()))
+        print("No greeting found for festival {} is not supported with our protocol".upper().format(festival.upper()))
 
 
 def is_corrupt(packet):
@@ -51,9 +39,11 @@ def is_corrupt(packet):
     seq_num = packet[0:4]
     ack_flag = packet[4:8]
     pay_len = packet[8:12]
-    payload = packet[12:32]
+    payloadLength = int.from_bytes(pay_len, byteorder='big')
+    # Added 12 to payloadLength.
+    payload = packet[12:payloadLength + 12]
 
-    check_sum = packet[32:].decode("ascii")
+    check_sum = packet[payloadLength + 12:]
     packet_to_check = seq_num + ack_flag + pay_len + payload
 
     # Sequence number, Ack flag, payload length, payload, checkSum
@@ -71,14 +61,14 @@ def is_corrupt(packet):
 def checksum(pkt):
     h = hashlib.new('md5')
     h.update(pickle.dumps(pkt))
-
-    return h.hexdigest()
+    # Checksum wont match because of hexdigest.
+    return h.digest()
 
 
 def create_packet(seq, ack, pay_load):
     # Creating packet with the format
     # Creating acknowledgement packet
-
+    pay_len = len(pay_load)
     seq_num = seq
     ack_flag = ack
     payload = pay_load
@@ -86,10 +76,8 @@ def create_packet(seq, ack, pay_load):
 
     # Need to pad the payload with extra characters since it is not at the maximum number of bytes
     # Padding payload with " " character
-    if len(payload) < 20:
-        temp = len(payload)
-        for i in range(temp, 20, 1):
-            payload = payload + " "
+
+    # Removed method to add extra characters.
 
     payload = payload.encode("ascii")
 
@@ -103,7 +91,7 @@ def create_packet(seq, ack, pay_load):
     pkt_no_checksum = seq_num + ack_flag + pay_len + payload
     checksum_num = checksum(pkt_no_checksum)
 
-    checksum_num = checksum_num.encode('ascii')
+    # checksum_num = checksum_num.encode('ascii')
 
     # Concatenate all together to create packet
     pkt = seq_num + ack_flag + pay_len + payload + checksum_num
@@ -122,44 +110,45 @@ def is_ack(packet):
         return False
 
 
-def seq_mismatch(packet):
-    # Sequence number, Ack flag, payload length, payload, checkSum
-
-    seq = packet[:4]
-
-    if seq != 0:
-        return True
-    else:
-        return False
-
-
 while True:
     # Receiving packet from the client
-    client_data = serverSocket.recvfrom(1024)
+    client_data = serverSocket.recvfrom(2048)
 
     # Data client has sends both the packet and the address it has received it from
     # packet from client
     packet = client_data[0]
 
+    # Saving the sequence number
+    seq_num = int.from_bytes(packet[0:4], byteorder="big")
+
+    # Saving payload length
+    pay_len = int.from_bytes(packet[8:12], byteorder="big")
+
+    # Adding sequence number and payload length for the acknowledgement sequence number
+    ack_seq_num = seq_num + pay_len
+
     # save the address it has received it from
     address = client_data[1]
-    print(address)
+    ip = address[0]
 
     # Checking that the packet received is valid and that it is not corrupt
     # If is_corrupt is false then the packet is in its correct state
 
     if not is_corrupt(packet):
-        print("[FESTIVAL REQUEST RECEIVED]")
-        print("[SENDING ACKNOWLEDGEMENT PACKET]")
+        print("[FESTIVAL REQUEST RECEIVED]: {}".format(ip))
+        # Checking if the received festival is in the list
+        festival_reply(packet[12:pay_len + 12].decode("ascii")[:pay_len])
+        print("[SENDING ACKNOWLEDGEMENT PACKET] to: {}".format(ip))
         # Sending acknowledgement packet to the client
-        serverSocket.sendto(create_packet(0, 1, " "), address)
+        serverSocket.sendto(create_packet(ack_seq_num, 1, ""), address)
 
     else:
-        print("[CORRUPTED]")
+        print("[PACKET IS CORRUPTED]")
 
     # Send greeting
     pay_len = int.from_bytes(packet[8:12], "big")
-    greeting = festival_reply(packet[12:32].decode("ascii")[:pay_len])
+    # Changed to pay_len + 12
+    greeting = festival_reply(packet[12:pay_len + 12].decode("ascii")[:pay_len])
     print("[SENDING GREETING]")
 
-    serverSocket.sendto(create_packet(0, 0, greeting), address)
+    serverSocket.sendto(create_packet(seq_num, 0, greeting), address)
